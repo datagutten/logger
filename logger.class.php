ab53883d7f46c201f07f2856e91d13178597d8c0
datagutten <datagutten@datagutten.net>
2019-07-22 21:11:12 +0200

    Re-set $dir after logpath fallback

--- logger.class.php
+++ logger.class.php
@@ -18,6 +18,7 @@ class logger
         {
             $filesystem->mkdir('logs');
             $this->logpath = realpath('logs');
+            $dir="{$this->logpath}/$subdir/".date('Y-m');
         }
 
 		$this->fp=fopen($dir.'/'.date('Y-m-d').'.txt','a+'); //Open log file for appending
